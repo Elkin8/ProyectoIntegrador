9ca1dfc5ab3a9043f2d4c628a627747c7b28568b
Elkin Guamán <edguamanc@puce.edu.ec>
2025-02-05 00:35:57 -0500

    CSShome

--- front-end/src/components/Navbar1.jsx
+++ front-end/src/components/Navbar1.jsx
@@ -4,17 +4,34 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    localStorage.removeItem("token"); // Elimina el token almacenado
-    navigate("/login"); // Redirige al usuario a la página de login
+    localStorage.removeItem("token");
+    navigate("/login");
   };
 
   return (
-    <nav>
-      <button onClick={handleLogout}>Cerrar Sesión</button>
-    </nav>
+      <nav class="navbar">
+
+        <div>
+          <h1>Bienvenido a nuestros retos</h1>
+        </div>
+
+        <div class="container-fluid w-100 p-0"> 
+          <div class="row g-0"> 
+            <div class="col text-center border-end py-2"> 
+              <button><p>Menú principal</p></button>
+            </div>
+            <div class="col text-center border-end py-2"> 
+              <button><p>Contáctanos</p></button>
+            </div>
+            <div class="col text-center py-2"> 
+              <button onClick={handleLogout}>
+                <p>Cerrar Sesión</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
   );
 };
 
 export default Navbar;
-
-
